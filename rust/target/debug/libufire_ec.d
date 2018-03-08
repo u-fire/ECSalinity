c314aa8a8cd18c4ad52e9635b6f13dd26c5b33d9
Justin Decker <justind000@gmail.com>
2018-03-07 18:53:06 -0800

    updates

--- rust/target/debug/libufire_ec.d
+++
@@ -1,1 +0,0 @@
-/home/justin/Development/ec-class/lib/ECSalinity/rust/target/debug/libufire_ec.rlib: /home/justin/Development/ec-class/lib/ECSalinity/rust/src/lib.rs
--- rust/target/debug/ufire_ec_bin.d
+++
@@ -1,1 +0,0 @@
-/home/justin/Development/ec-class/lib/ECSalinity/rust/target/debug/ufire_ec_bin: /home/justin/Development/ec-class/lib/ECSalinity/rust/src/main.rs /home/justin/Development/ec-class/lib/ECSalinity/rust/src/lib.rs
